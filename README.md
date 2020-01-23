5d11a8e15922dda3558a8bd27c5ad58045a6fcd5
SamAwo <55980014+SamuelAwojuola@users.noreply.github.com>
2020-01-23 21:31:05 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Prophecy
+Compilation of files used during the Dec 2019 Retreat.
---
+++ jscript/colmnJS.js
@@ -0,0 +1,124 @@
+// parseInt()
+
+$(document).ready(function () {
+
+	var ArrayOfAllTds = document.getElementsByTagName("td");
+	var customIndex = 0;
+	var customIndex = customIndex++;
+
+	//TO ADD CUSTOM CLASSES TO THE <TD>
+	for (var i = 0; i < ArrayOfAllTds.length; i++) {
+		var tdsIndexInItsRow = ArrayOfAllTds[i].cellIndex;
+		var TD = $("td")[i];
+
+		//FOR FIRST <td> IN A ROW
+		if (tdsIndexInItsRow == 0) {
+			var colSpan = tdsIndexInItsRow + 1;
+			var colClass = "col-" + colSpan;
+			$(TD).addClass(colClass);
+			customIndex = 1;
+			//			alert(customIndex);
+
+
+
+			//FOR IF THE FIRST <td> IN THE ROW HAS A "colspan" ATTRIBUTE
+			if (TD.hasAttribute("colspan")) {
+				var colSpan2 = $(TD).attr("colspan");
+
+				for (var cI = 1; cI < colSpan2; cI++) {
+					var colSpan = cI + 1;
+					var colClass = "col-" + colSpan;
+					$(TD).addClass(colClass);
+					customIndex = customIndex + 1;
+					//TO SAVE THE ORGINAL COLSPAN AS ANOTHER MADE-UP ATTRIBUTE
+					$(TD).attr("originalcolspan", colSpan);
+
+				}
+			}
+		}
+
+		//FOR OTHER <td> IN A ROW
+		else {
+
+			var colSpan = customIndex + 1;
+			var colClass = "col-" + colSpan;
+			$(TD).addClass(colClass);
+			customIndex = customIndex + 1;
+			//			alert(customIndex);
+
+
+			//FOR IF THE OTHER <td>s APART FROM THE FIRST IN THE ROW HAS A "colspan" ATTRIBUTE
+			if (TD.hasAttribute("colspan")) {
+				var colSpan2 = $(TD).attr("colspan");
+
+				//TO SAVE THE ORGINAL COLSPAN AS ANOTHER MADE-UP ATTRIBUTE
+				$(TD).attr("originalcolspan", colSpan2);
+
+				for (var cI = 1; cI < colSpan2; cI++) {
+					var colSpan = customIndex + 1;
+					var colClass = "col-" + colSpan;
+					$(TD).addClass(colClass);
+					customIndex = customIndex + 1;
+				}
+			}
+		}
+	}
+
+	//TO SHOW ONE COL-X AT A TIME ON BUTTON CLICK
+	//NEXT BUTTON SHOWS PREVIOUS COLUMN
+	var next = 0;
+	var soloTD = customIndex + 1;
+	$("#nextBtn").click(function () {
+		next = next + 1;
+		if (next < soloTD) {
+			var col2show = ".col-" + next;
+			$("td").hide();
+			// $("td").not("td:first-child").hide();
+			$(col2show).show();
+			$(col2show).attr("colspan", "1");
+
+		} else if (next == soloTD) {
+			$("td").show();
+			next = 0;
+
+			TableRebuild();
+		}
+	});
+
+	//PREV BUTTON SHOWS PREVIOUS COLUMN
+	$("#prevBtn").click(function () {
+		if (next > 1) {
+			var prevCol2show = ".col-" + --next;
+			$("td").hide();
+			//			$("td").not("td:first-child").hide();
+			$(prevCol2show).show();
+		}
+		if (next == 1) {
+			$("td").show();
+			//			next = 0;
+			TableRebuild();
+		}
+	});
+
+
+	//TO ADD CUSTOM CLASSES TO THE <TD>
+	$("#refreshBtn").click(function () {
+		TableRebuild();
+	});
+
+	//FUNCTION FOR RESTORING THE ORIGINAL COLSPAN VALUES OF THE <TD>s
+	function TableRebuild() {
+		var ci = 0
+		while (ci < ArrayOfAllTds.length) {
+			//			var tdsIndexInItsRow = ArrayOfAllTds[i].cellIndex;
+			var TDee = $("td")[ci];
+			if (TDee.hasAttribute("originalcolspan")) {
+				var OrgColSpan = $(TDee).attr("originalcolspan");
+				$(TDee).attr("colspan", OrgColSpan);
+				$("td").show();
+				next = 0;
+			};
+			ci = ci + 1;
+		}
+	}
+});
